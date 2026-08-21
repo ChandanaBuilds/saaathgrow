5b9e3fb291dc081e774a0f0d51e00a701f647b92
ChandanaBuildse <chandanagoudt2408@gmail.com>
2026-08-21 11:07:56 +0530

    changed database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,18 +1,30 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./saathGroww.db"
+
+DATABASE_URL = os.getenv("postgresql://saath_groww_db_user:KyXKoTmkTc5mVlbXp1MlwLPQjUx4O4EE@dpg-da3u3s2jobas739pdli0-a/saath_groww_db")
+
+
+if not DATABASE_URL:
+    raise RuntimeError(
+        "DATABASE_URL environment variable is not set."
+    )
+
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
